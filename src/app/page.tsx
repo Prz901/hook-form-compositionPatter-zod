f4480b46e4da4e14e86aa18559f87d99f08eae29
prz901 <felipeperozo@hotmail.com>
2024-01-28 17:26:05 -0300

    feat: create a form with react-hook-form, zod for validation and use composition pattern to understand how to create a component with this pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,176 @@
-import Image from "next/image";
+'use client'
+
+import { useForm, FormProvider, useFieldArray } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form } from '@/components/Form'
+
+/**
+ * To - do
+ * [x] - Validação /transformação
+ * [x] - Field Arrays
+ * [ ] - Upload de arquivos - Supabase
+ * [ ] - Composition Pattern
+ */
+
+
+const createUseFormSchema = z.object({
+  name: z.string().min(1, "O nome é obrigatório").transform(name => {
+    return name.trim().split(' ').map(word => {
+      return word[0].toLocaleUpperCase().concat(word.substring(1))
+    }).join(' ')
+  }),
+  email: z.string().min(1, 'O e-mail é obrigatório').email('Formato de e-mail inválido').refine(email => {
+    return email.endsWith('@rocketseat.com.br')
+  }, 'O e-mail precisa ser da Rocketseat'),
+  password: z.string().min(6, 'A senha precisa de no mínimo 6 caracteres'),
+  techs: z.array(z.object({
+    title: z.string().min(1, 'título é obrigatório'),
+    knowledge: z.coerce.number().min(1).max(100)
+  })).min(2, 'Insira pelo menos 2 tecnologias')
+})
+
+type CreateUserFormData = z.infer<typeof createUseFormSchema>
 
 export default function Home() {
+
+  const createUserForm = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUseFormSchema)
+  })
+
+  // const { register, handleSubmit, formState: { errors }, control } = useForm<CreateUserFormData>({
+  //   resolver: zodResolver(createUseFormSchema)
+  // })
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+    control
+  } = createUserForm
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'techs'
+  })
+
+  function createUser(data: any) {
+    console.log("data", data)
+  }
+
+  function addNewTech() {
+    append({ title: "", knowledge: 0 })
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className="min-h-screen bg-zinc-900 flex items-center justify-center text-white"  >
+      <FormProvider {...createUserForm}>
+        <form
+          className="flex flex-col gap-4 w-full max-w-xs"
+          onSubmit={handleSubmit(createUser)}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <Form.Field>
+            <Form.Label htmlFor='name'>
+              Nome
+            </Form.Label>
+            <Form.Input name='name' type='name' />
+            <Form.ErrorMessage field='name' />
+          </Form.Field>
+          <Form.Field>
+            <Form.Label htmlFor='email'>
+              E-mail
+            </Form.Label>
+            <Form.Input name='email' type='email' />
+            <Form.ErrorMessage field='email' />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          </Form.Field>
+          <Form.Field>
+            <Form.Label htmlFor='password'>
+              Password
+            </Form.Label>
+            <Form.Input name='password' type='password' />
+            <Form.ErrorMessage field='password' />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          </Form.Field>
+          <Form.Field>
+            <Form.Label>
+              Tecnologias
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              <button
+                type="button"
+                onClick={addNewTech}
+                className="text-emerald-500 font-semibold text-xs flex items-center gap-1"
+              >
+                Adicionar nova
+              </button>
+            </Form.Label>
+            <Form.ErrorMessage field="techs" />
+
+            {fields.map((field, index) => {
+              const fieldName = `techs.${index}.title`
+              const fieldKnowlegde = `techs.${index}.knowledge`
+
+
+              return (
+
+                <Form.Field key={field.id}>
+                  <div className="flex gap-2 items-center">
+                    <div>
+                      <Form.Input type={fieldName} name={fieldName} />
+                      <Form.ErrorMessage field={fieldName} />
+                    </div>
+                    <div>
+                      <Form.Input type={fieldKnowlegde} name={fieldKnowlegde} />
+                      <Form.ErrorMessage field={fieldKnowlegde} />
+                    </div>
+
+                  </div>
+                </Form.Field>
+
+
+              )
+            })}
+          </Form.Field>
+          {/* <div className="flex flex-col gap-1">
+            <label htmlFor="" className='flex items-center justify-between'>
+              Tenologias
+
+              <button
+                onClick={addNewTech}
+                className='text-emerald-500 text-sm'
+              >
+                Adicionar
+              </button>
+
+            </label>
+
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id} className='flex gap-2'>
+                  <div className='flex flex-1 flex-col gap-1'>
+                    <input
+                      type="text"
+                      className=" border border-zinc-600 bg-zinc-800 shadow-sm rounded h-10 px-3 outline-none"
+                      {...register(`techs.${index}.title`)}
+                    />
+                    {errors.techs?.[index]?.title && <span className='text-red-500 text-sm'>{errors.techs?.[index]?.title?.message}</span>}
+                  </div>
+                  <div className='flex flex-1 flex-col gap-1'>
+                    <input
+                      type="number"
+                      className="w-16 border border-zinc-600 bg-zinc-800 shadow-sm rounded h-10 px-3 outline-none"
+                      {...register(`techs.${index}.knowledge`)}
+                    />
+
+                    {errors.techs?.[index]?.knowledge && <span className='text-red-500 text-sm'>{errors.techs?.[index]?.knowledge?.message}</span>}
+                  </div>
+                </div>
+              )
+            })}
+            {errors.techs && <span className='text-red-500 text-sm'>{errors.techs.message}</span>}
+          </div> */}
+          <button type="submit" className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600">Salvar</button>
+        </form>
+      </FormProvider>
     </main>
   );
 }
